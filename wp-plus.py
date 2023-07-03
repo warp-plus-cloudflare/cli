1f529ae7a1a40821149e489ad60baced1fcfa33f
ALIILAPRO <babab7526@gmail.com>
2023-07-04 02:39:22 +0330

    Support Proxy beta version
    The ability to use proxy was added.

--- wp-plus.py
+++ wp-plus.py
@@ -7,31 +7,48 @@ import time
 import os
 import sys
 import pathlib
-script_version = '4.0.0'
-window_title   = f"WARP-PLUS-CLOUDFLARE By ALIILAPRO (version {script_version})"
-os.system('title ' + window_title if os.name == 'nt' else 'PS1="\[\e]0;' + window_title + '\a\]"; echo $PS1')
+
+
+script_version = '4.1.0 beta'
+window_title = f"WARP-PLUS-CLOUDFLARE By ALIILAPRO (version {script_version})"
+os.system('title ' + window_title if os.name ==
+          'nt' else 'PS1="\[\e]0;' + window_title + '\a\]"; echo $PS1')
 os.system('cls' if os.name == 'nt' else 'clear')
-print ("            *********************************")
-print ("            WARP-PLUS-CLOUDFLARE By ALIILAPRO")
-print ("            *********************************")
-print ("")
-print ("[+] ABOUT SCRIPT:")
-print ("[-] With this script, you can getting unlimited GB on Warp+.")
-print (f"[-] Version: {script_version}")
-print ("--------")
-print ("[+] THIS SCRIPT CODDED BY ALIILAPRO") 
-print ("[-] SITE: aliilapro.github.io") 
-print ("[-] TELEGRAM: aliilapro")
-print ("--------")
+print("            *********************************")
+print("            WARP-PLUS-CLOUDFLARE By ALIILAPRO")
+print("            *********************************")
+print("")
+print("[+] ABOUT SCRIPT:")
+print("[-] With this script, you can getting unlimited GB on Warp+.")
+print(f"[-] Version: {script_version}")
+print("--------")
+print("[+] THIS SCRIPT CODDED BY ALIILAPRO")
+print("[-] SITE: aliilapro.github.io")
+print("[-] TELEGRAM: aliilapro")
+print("--------")
+
+
+proxy_url = 'https://raw.githubusercontent.com/ALIILAPRO/Proxy/main/http.txt'
+response = urllib.request.urlopen(proxy_url)
+proxy_list = response.read().decode('utf-8').splitlines()
+
+proxy_file = 'proxy.txt'
+with open(proxy_file, 'w') as file:
+	for proxy in proxy_list:
+		file.write(proxy + '\n')
+
+with open(proxy_file, 'r') as file:
+	proxy_list = [line.strip() for line in file]
+
 
 def newID():
 	while True:
-		referrer  = input("[#] Enter the WARP+ ID:")
+		referrer = input("[#] Enter the WARP+ ID: ")
 		user_input = input(f"[?] Your ID = ({referrer}) is it correct? (y/n):")
 		if user_input == "y":
 			save_id = input("[?] Do you want to save your ID? (y/n):")
 			if save_id == "y":
-				with open("referrer.txt","w") as file:
+				with open("referrer.txt", "w") as file:
 					file.write(referrer)
 				return referrer
 			elif save_id == "n":
@@ -43,15 +60,18 @@ def newID():
 		else:
 			print(f"\"{user_input}\" is not a valid parameter.")
 
+
 def progressBar():
-	animation     = ["[□□□□□□□□□□]","[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]"]
+	animation = ["[□□□□□□□□□□]", "[■□□□□□□□□□]", "[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]",
+              "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]"]
 	progress_anim = 0
-	save_anim     = animation[progress_anim % len(animation)]
-	percent       = 0
+	save_anim = animation[progress_anim % len(animation)]
+	percent = 0
 	while True:
 		for i in range(10):
 			percent += 1
-			sys.stdout.write(f"\r[+] Waiting response...  " + save_anim + f" {percent}%")
+			sys.stdout.write(f"\r[+] Waiting response...  " +
+			                 save_anim + f" {percent}%")
 			sys.stdout.flush()
 			time.sleep(0.075)
 		progress_anim += 1
@@ -60,50 +80,57 @@ def progressBar():
 			sys.stdout.write("\r[+] Request completed... [■■■■■■■■■■] 100%")
 			break
 
+
 def genString(stringLength):
 	try:
 		letters = string.ascii_letters + string.digits
 		return ''.join(random.choice(letters) for i in range(stringLength))
 	except Exception as error:
-		print(error)		    
+		print(error)
+
+
 def digitString(stringLength):
 	try:
 		digit = string.digits
-		return ''.join((random.choice(digit) for i in range(stringLength)))    
+		return ''.join((random.choice(digit) for i in range(stringLength)))
 	except Exception as error:
-		print(error)	
+		print(error)
+
 url = f'https://api.cloudflareclient.com/v0a{digitString(3)}/reg'
+
+
 def run():
 	try:
 		install_id = genString(22)
 		body = {"key": "{}=".format(genString(43)),
-				"install_id": install_id,
-				"fcm_token": "{}:APA91b{}".format(install_id, genString(134)),
-				"referrer": referrer,
-				"warp_enabled": False,
-				"tos": datetime.datetime.now().isoformat()[:-3] + "+02:00",
-				"type": "Android",
-				"locale": "es_ES"}
+                    "install_id": install_id,
+                    "fcm_token": "{}:APA91b{}".format(install_id, genString(134)),
+                    "referrer": referrer,
+                    "warp_enabled": False,
+                    "tos": datetime.datetime.now().isoformat()[:-3] + "+02:00",
+                    "type": "Android",
+                    "locale": "es_ES"}
 		data = json.dumps(body).encode('utf8')
 		headers = {'Content-Type': 'application/json; charset=UTF-8',
-					'Host': 'api.cloudflareclient.com',
-					'Connection': 'Keep-Alive',
-					'Accept-Encoding': 'gzip',
-					'User-Agent': 'okhttp/3.12.1'
-					}
-		req         = urllib.request.Request(url, data, headers)
-		response    = urllib.request.urlopen(req)
-		status_code = response.getcode()	
+                    'Host': 'api.cloudflareclient.com',
+                    'Connection': 'Keep-Alive',
+                    'Accept-Encoding': 'gzip',
+                    'User-Agent': 'okhttp/3.12.1'
+             }
+		req = urllib.request.Request(url, data, headers)
+		response = urllib.request.urlopen(req)
+		status_code = response.getcode()
 		return status_code
 	except Exception as error:
 		print("")
-		print(error)	
+		print(error)
+
 
 if pathlib.Path("referrer.txt").exists():
 	while True:
 		user_input = input("[?] Do you want to use saved WARP+ ID? (y/n):")
 		if user_input == "y":
-			with open("referrer.txt","r") as file:
+			with open("referrer.txt", "r") as file:
 				referrer = file.read().strip()
 			break
 		elif user_input == "n":
@@ -116,30 +143,26 @@ else:
 
 g = 0
 b = 0
-while True:
+
+for proxy_url in proxy_list:
 	os.system('cls' if os.name == 'nt' else 'clear')
-	print ("            *********************************")
-	print ("            WARP-PLUS-CLOUDFLARE By ALIILAPRO")
-	print ("            *********************************")
-	print ("")
-	sys.stdout.write("\r[+] Sending request...   [□□□□□□□□□□] 0%")
-	sys.stdout.flush()
-	result = run()
-	if result == 200:
-		g += 1
-		progressBar()
-		print(f"\n[-] WORK ON ID: {referrer}")    
-		print(f"[:)] {g} GB has been successfully added to your account.")
-		print(f"[#] Total: {g} Good {b} Bad")
-		for i in range(18,0,-1):
-			sys.stdout.write(f"\r[*] After {i} seconds, a new request will be sent.")
-			sys.stdout.flush()
-			time.sleep(1)
-	else:
-		b += 1
-		print("[:(] Error when connecting to server.")
-		print(f"[#] Total: {g} Good {b} Bad")
-		for i in range(10,0,-1):
-			sys.stdout.write(f"\r[*] Retrying in {i}s...")
-			sys.stdout.flush()
-			time.sleep(1)
+	print("            *********************************")
+	print("            WARP-PLUS-CLOUDFLARE By ALIILAPRO")
+	print("            *********************************")
+	print("")
+	try:
+		print(f"[#] Total: Good {g}  Bad {b}")
+		sys.stdout.write("\r[+] Sending request...   [□□□□□□□□□□] 0%")
+		sys.stdout.flush()
+		proxy_handler = urllib.request.ProxyHandler(
+			{'http': proxy_url, 'https': proxy_url})
+		opener = urllib.request.build_opener(proxy_handler)
+		urllib.request.install_opener(opener)
+		result = run()
+		if result == 200:
+			g += 1
+			progressBar()
+		else:
+			b += 1
+	except Exception as e:
+		print(f"Proxy: {proxy_url} - Error: {str(e)}")
